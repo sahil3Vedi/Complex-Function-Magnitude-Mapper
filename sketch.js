66677891f31115846207bc032068f2347f3c1c91
sahil3Vedi <st9800@gmail.com>
2017-10-17 13:36:17 +0530

    Labelling complete. Functional graph

--- sketch.js
+++ sketch.js
@@ -10,10 +10,10 @@ function bar(min,max,x,y,len){
   }
 }
 
-function scroller(bar,min,max){
+function scroller(bar,min,max,fac){
   this.min=min;
   this.max=max;
-  this.x=bar.x+(bar.len/2);
+  this.x=bar.x+(bar.len*fac);
   this.y=bar.y;
   this.value=5;
   this.show=function(){
@@ -47,13 +47,23 @@ function complex(x,y){
 var mode=1;
 var i;
 var j;
-var maxz=0;
 
 var ResolutionB;
 var ResolutionS;
+var MinXB;
+var MinXS;
+var MaxXB;
+var MaxXS;
+var MinYB;
+var MinYS;
+var MaxYB;
+var MaxYS;
 
 var res;
 var minX;
+var maxX;
+var minY;
+var maxY;
 
 
 
@@ -62,11 +72,20 @@ var minX;
 function setup() {
   createCanvas(windowWidth,windowHeight);
   ResolutionB = new bar(1,10,-600,-300,100);
-  ResolutionS = new scroller(ResolutionB,1,10);
+  ResolutionS = new scroller(ResolutionB,1,10,0.5);
   res=int(ResolutionS.value);
-  MinXB = new bar(-windowWidth/2,windowWidth/2,-600,-250,100);
-  MinXS = new scroller(MinXB,-windowWidth/2,windowWidth/2);
+  MinXB = new bar(-windowHeight/2,windowHeight/2,-600,-250,100);
+  MinXS = new scroller(MinXB,-windowHeight/2,windowHeight/2,0.25);
   minX=int(MinXS.value);
+  MaxXB = new bar(-windowHeight/2,windowHeight/2,-600,-200,100);
+  MaxXS = new scroller(MaxXB,-windowHeight/2,windowHeight/2,0.75);
+  maxX=int(MaxXS.value);
+  MinYB = new bar(-windowWidth/2,windowWidth/2,-600,-150,100);
+  MinYS = new scroller(MinYB,-windowWidth/2,windowWidth/2,0.25);
+  minY=int(MinYS.value);
+  MaxYB = new bar(-windowWidth/2,windowWidth/2,-600,-100,100);
+  MaxYS = new scroller(MaxYB,-windowWidth/2,windowWidth/2,0.75);
+  maxY=int(MaxYS.value);
 }
 
 function keyPressed() {
@@ -79,9 +98,27 @@ function keyPressed() {
   } else if (key == 'W' || key == 'w') {
     MinXS.x-=1;
     minX=int(MinXS.value);
-  } else if (key == 'S' || key == 'S') {
+  } else if (key == 'S' || key == 's') {
     MinXS.x+=1;
     minX=int(MinXS.value);
+  } else if (key == 'I' || key == 'i') {
+    MaxXS.x-=1;
+    maxX=int(MaxXS.value);
+  } else if (key == 'K' || key == 'k') {
+    MaxXS.x+=1;
+    maxX=int(MaxXS.value);
+  } else if (key == 'L' || key == 'l') {
+    MinYS.x-=1;
+    minY=int(MinYS.value);
+  } else if (key == 'J' || key == 'j') {
+    MinYS.x+=1;
+    minY=int(MinYS.value);
+  } else if (key == 'D' || key == 'd') {
+    MaxYS.x-=1;
+    maxY=int(MaxYS.value);
+  } else if (key == 'A' || key == 'a') {
+    MaxYS.x+=1;
+    maxY=int(MaxYS.value);
   }
   return false; // prevent default
 }
@@ -97,11 +134,14 @@ function draw(){
     //if w=z
     if(mode==1){
       let maxz=0;
-      for(i=minX;i<21;i+=res){
-        for(j=-20;j<21;j+=res){
+      for(i=minX;i<maxX;i+=res){
+        for(j=minY;j<maxY;j+=res){
           let z=new complex(i,j);
-          maxz=(new complex(20,20)).mag()
-          let hue=Math.floor(map(z.mag(),0,maxz,0,255));
+          let m=z.mag();
+          if(m>maxz){
+            maxz=m;
+          }
+          let hue=Math.floor(map(m,0,maxz,0,255));
           stroke(255-hue,hue,0);
           point(i,j);
         }
@@ -137,8 +177,19 @@ function draw(){
   stroke(255);
   noFill();
   rotate(-PI/2);
-  text(Math.floor(frameRate()), 100, 100);
-  //labelling graphs
+  text('FrameRate: '+ Math.floor(frameRate()), 500, -270);
+  text('type W-A-L-K to get started',500,-300);
+  text('Q',-620,-285);
+  text('E - Resolution',-490,-285);
+  text('W',-620,-235);
+  text('S - Ymax',-490,-235);
+  text('I',-615,-185);
+  text('K - Ymin',-490,-185);
+  text('L',-620,-135);
+  text('J - Xmax',-490,-135);
+  text('D',-620,-85);
+  text('A - Xmin',-490,-85);
+  //labelling graphs50
   for(i=-windowWidth/2;i<windowWidth/2;i++){
     if(i%50==0){
       text(i,i,0);
@@ -153,5 +204,11 @@ function draw(){
   ResolutionS.show();
   MinXB.show();
   MinXS.show();
+  MaxXB.show();
+  MaxXS.show();
+  MinYB.show();
+  MinYS.show();
+  MaxYB.show();
+  MaxYS.show();
 
 }
